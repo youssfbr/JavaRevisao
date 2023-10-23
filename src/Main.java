981961a47c6a0e91f2a14f0a8cc3d324bcb0f780
Alisson Youssf <youssfbr@gmail.com>
2023-10-23 12:53:11 -0300

    Ternario

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int media = 30;
+        //System.out.println(numero);
+        String resultado;
+
+        resultado = media >= 70 ? "Aprovado" :
+                (media >= 40) ? "Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
+        
+    }
+}
